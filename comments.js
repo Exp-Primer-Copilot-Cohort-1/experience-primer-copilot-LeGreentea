b77ae099778984c8657f0ee3232330f8211599a9
Dero Ivano <125928012+LeGreentea@users.noreply.github.com>
2023-12-03 14:25:54 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,73 +1,36 @@
-// create web server
-// run: node comments.js
-// open browser and go to http://localhost:3000
-// to stop running: ctrl+c
+// Create web server
+// 1. Create a web server
+// 2. Create a route for the home page
+// 3. Create a route for the about page
+// 4. Create a route for the contact us page
+// 5. Create a route for the 404 page
+// 6. Start and listen on port 3000
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+// 1. Create a web server
+const express = require('express');
+const app = express();
 
-var comments = [];
-
-var server = http.createServer(function(req, res) {
-  var url_parts = url.parse(req.url);
-  console.log(url_parts);
-  switch (url_parts.pathname) {
-    case '/':
-      fs.readFile('./comments.html', function(err, data) {
-        res.end(data);
-      });
-      break;
-    case '/comments':
-      if (req.method == 'POST') {
-        var reqBody = '';
-        req.on('data', function(data) {
-          reqBody += data;
-        });
-        req.on('end', function() {
-          var comment = JSON.parse(reqBody);
-          comments.push(comment);
-          res.end();
-        });
-      } else if (req.method == 'GET') {
-        var commentsToReturn = [];
-        var query = url_parts.query;
-        if (query != '') {
-          var commentsQuery = JSON.parse(query);
-          for (var i = 0; i < comments.length; i++) {
-            var comment = comments[i];
-            if (comment.name == commentsQuery.name) {
-              commentsToReturn.push(comment);
-            }
-          }
-        } else {
-          commentsToReturn = comments;
-        }
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(JSON.stringify(commentsToReturn));
-      }
-      break;
-    case '/comments.html':
-      fs.readFile('./comments.html', function(err, data) {
-        res.end(data);
-      });
-      break;
-    case '/comments.css':
-      fs.readFile('./comments.css', function(err, data) {
-        res.end(data);
-      });
-      break;
-    case '/comments.js':
-      fs.readFile('./comments.js', function(err, data) {
-        res.end(data);
-      });
-      break;
-    default:
-      res.statusCode = 404;
-      res.end('Not found');
-  }
+// 2. Create a route for the home page
+app.get('/', (req, res) => {
+  res.send('This is the home page');
 });
 
-server.listen(3000, function() {
-  console.log('Server running on port 3000');
+// 3. Create a route for the about page
+app.get('/about', (req, res) => {
+  res.send('This is the about page');
+});
+
+// 4. Create a route for the contact us page
+app.get('/contact', (req, res) => {
+  res.send('This is the contact us page');
+});
+
+// 5. Create a route for the 404 page
+app.use((req, res) => {
+  res.status(404).send('Not found');
+});
+
+// 6. Start and listen on port 3000
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
